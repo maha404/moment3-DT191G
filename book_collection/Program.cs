33ccdd2867f75d72dcff802a32306d3c42f96eee
Your Name <you@example.com>
2024-02-09 15:32:05 +0100

    Ändrade koden då databas kopplingen låg fel.

--- book_collection/Program.cs
+++ book_collection/Program.cs
@@ -3,6 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); // MVC
+
+// Databas
+builder.Services.AddDbContext<BookContext> (options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
+);
+
 var app = builder.Build();
 
 app.UseStaticFiles(); // wwwroot
@@ -11,12 +17,7 @@ app.UseRouting();
 // Routing
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
-);
-
-// Databas
-builder.Services.AddDbContext<AuthorContext>(options => 
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
+    pattern: "{controller=Book}/{action=Index}/{id?}"
 );
 
 app.Run();
